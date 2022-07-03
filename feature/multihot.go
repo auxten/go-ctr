f45d15fd45abedf1db2dd0e7ea42867cdb67c9e1
auxten <auxtenwpc@gmail.com>
2022-07-03 15:39:39 +0800

    Err handling
    Signed-off-by: auxten <auxtenwpc@gmail.com>

--- feature/multihot.go
+++ feature/multihot.go
@@ -27,7 +27,10 @@ func StringSplitMultiHot(str string, sep string, size int) []float64 {
 	result := make([]float64, size)
 	for _, s := range strings.Split(str, sep) {
 		hash := fnv.New32()
-		hash.Write([]byte(strings.ToLower(s)))
+		_, err := hash.Write([]byte(strings.ToLower(s)))
+		if err != nil {
+			return nil
+		}
 		result[int(hash.Sum32())%size] = 1
 	}
 	return result
