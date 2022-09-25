8c2eed67b43289ce2491c337937bc0f04d4bef11
auxten <auxtenwpc@gmail.com>
2022-09-25 22:38:27 +0800

    Din and simple MLP training test

--- model/din/din_test.go
+++ model/din/din_test.go
@@ -13,50 +13,50 @@ import (
 )
 
 func TestDin(t *testing.T) {
-	Convey("Din simple Grad", t, func() {
-		log.SetLevel(log.DebugLevel)
-		rand.Seed(42)
-		var (
-			batchSize     = 100
-			uProfileDim   = 9
-			uBehaviorSize = 3
-			uBehaviorDim  = 7
-			iFeatureDim   = 7
-			cFeatureDim   = 9
+	log.SetLevel(log.DebugLevel)
+	rand.Seed(42)
+	var (
+		batchSize     = 100
+		uProfileDim   = 5
+		uBehaviorSize = 3
+		uBehaviorDim  = 7
+		iFeatureDim   = 7
+		cFeatureDim   = 5
 
-			numExamples = 10000
-			epochs      = 200
-			sampleInfo  = &rcmd.SampleInfo{
-				UserProfileRange:  [2]int{0, uProfileDim},
-				UserBehaviorRange: [2]int{uProfileDim, uProfileDim + uBehaviorSize*uBehaviorDim},
-				ItemFeatureRange:  [2]int{uProfileDim + uBehaviorSize*uBehaviorDim, uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim},
-				CtxFeatureRange:   [2]int{uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim, uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim + cFeatureDim},
-			}
-			inputWidth = uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim + cFeatureDim
-		)
-		inputSlice := make([]float64, numExamples*inputWidth)
-		for i := 0; i < numExamples; i++ {
-			for j := 0; j < sampleInfo.UserProfileRange[1]; j++ {
-				inputSlice[i*inputWidth+j] = rand.Float64()
-			}
-			for j := sampleInfo.CtxFeatureRange[0]; j < sampleInfo.CtxFeatureRange[1]; j++ {
-				inputSlice[i*inputWidth+j] = rand.Float64()
-			}
+		numExamples = 10000
+		epochs      = 20
+		sampleInfo  = &rcmd.SampleInfo{
+			UserProfileRange:  [2]int{0, uProfileDim},
+			UserBehaviorRange: [2]int{uProfileDim, uProfileDim + uBehaviorSize*uBehaviorDim},
+			ItemFeatureRange:  [2]int{uProfileDim + uBehaviorSize*uBehaviorDim, uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim},
+			CtxFeatureRange:   [2]int{uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim, uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim + cFeatureDim},
 		}
-		inputs := tensor.New(tensor.WithShape(numExamples, inputWidth), tensor.WithBacking(inputSlice))
-		labelSlice := make([]float64, numExamples)
-		for i := 0; i < numExamples; i++ {
-			//distance of uProfile and cFeature slice
-			var dist float64
-			for j := sampleInfo.UserProfileRange[0]; j < sampleInfo.UserProfileRange[1]; j++ {
-				dist += math.Abs(inputSlice[i*inputWidth+j] - inputSlice[i*inputWidth+j+sampleInfo.CtxFeatureRange[0]])
-			}
-			labelSlice[i] = math.Round(dist / float64(uProfileDim) * 1.5)
+		inputWidth = uProfileDim + uBehaviorSize*uBehaviorDim + iFeatureDim + cFeatureDim
+	)
+	inputSlice := make([]float64, numExamples*inputWidth)
+	for i := 0; i < numExamples; i++ {
+		for j := 0; j < sampleInfo.UserProfileRange[1]; j++ {
+			inputSlice[i*inputWidth+j] = rand.Float64()
 		}
-		labels := tensor.New(tensor.WithShape(numExamples, 1), tensor.WithBacking(labelSlice))
-		log.Debugf("labels: %+v", labels.Data())
+		for j := sampleInfo.CtxFeatureRange[0]; j < sampleInfo.CtxFeatureRange[1]; j++ {
+			inputSlice[i*inputWidth+j] = rand.Float64()
+		}
+	}
+	inputs := tensor.New(tensor.WithShape(numExamples, inputWidth), tensor.WithBacking(inputSlice))
+	labelSlice := make([]float64, numExamples)
+	for i := 0; i < numExamples; i++ {
+		//distance of uProfile and cFeature slice
+		var dist float64
+		for j := sampleInfo.UserProfileRange[0]; j < sampleInfo.UserProfileRange[1]; j++ {
+			dist += math.Abs(inputSlice[i*inputWidth+j] - inputSlice[i*inputWidth+j+sampleInfo.CtxFeatureRange[0]])
+		}
+		labelSlice[i] = math.Round(dist / float64(uProfileDim) * 1.5)
+	}
+	labels := tensor.New(tensor.WithShape(numExamples, 1), tensor.WithBacking(labelSlice))
+	log.Debugf("labels: %+v", labels.Data())
+
+	Convey("Din model", t, func() {
 		g := G.NewGraph()
-		//model := NewSimpleMLP(g, uProfileDim, uBehaviorSize, uBehaviorDim, iFeatureDim, cFeatureDim)
 		model := NewDinNet(g, uProfileDim, uBehaviorSize, uBehaviorDim, iFeatureDim, cFeatureDim)
 		err := Train(uBehaviorSize, uBehaviorDim, uProfileDim, iFeatureDim, cFeatureDim,
 			numExamples, batchSize, epochs,
@@ -66,6 +66,18 @@ func TestDin(t *testing.T) {
 			model,
 		)
 		So(err, ShouldBeNil)
+	})
 
+	Convey("Simple MLP", t, func() {
+		g := G.NewGraph()
+		model := NewSimpleMLP(g, uProfileDim, uBehaviorSize, uBehaviorDim, iFeatureDim, cFeatureDim)
+		err := Train(uBehaviorSize, uBehaviorDim, uProfileDim, iFeatureDim, cFeatureDim,
+			numExamples, batchSize, epochs,
+			sampleInfo,
+			inputs, labels,
+			g,
+			model,
+		)
+		So(err, ShouldBeNil)
 	})
 }
