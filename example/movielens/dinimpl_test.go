7cdcdfeb5249c94f474101842e453ec1b9905d88
auxten <auxtenwpc@gmail.com>
2022-10-26 23:28:35 +0800

    Fix log(1-prob)

--- example/movielens/dinimpl_test.go
+++ example/movielens/dinimpl_test.go
@@ -30,7 +30,7 @@ func TestDinOnMovielens(t *testing.T) {
 			predBatchSize: 100,
 			batchSize:     200,
 			epochs:        200,
-			earlyStop:     10,
+			earlyStop:     1,
 		}
 		trainCtx := context.Background()
 		model, err = rcmd.Train(trainCtx, movielens, dinModel)
