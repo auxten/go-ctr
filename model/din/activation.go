7f80e6e9ff48ac9ad6103083c268abcf5de76d84
auxten <auxtenwpc@gmail.com>
2022-10-28 17:34:12 +0800

    A nice EucDistance

--- model/din/activation.go
+++ model/din/activation.go
@@ -1,6 +1,7 @@
 package din
 
 import (
+	log "github.com/sirupsen/logrus"
 	G "gorgonia.org/gorgonia"
 )
 
@@ -10,5 +11,38 @@ func PRelu(x, slop *G.Node) (retVal *G.Node) {
 	negative := G.Must(G.HadamardProd(G.Must(G.Sub(x, G.Must(G.Abs(x)))), slop))
 	positive := G.Must(G.Add(x, G.Must(G.Abs(x))))
 	retVal = G.Must(G.Mul(G.Must(G.Add(negative, positive)), G.NewConstant(0.5)))
+	return
+}
+
+// EucDistance is the Euclidean distance between two matrix, typically used for
+// calculating the distance between two embedding.
+// Case1: x, y shapes are same, no broadcast. output shape will be x.shape[:-1]
+// Case2: x, y shapes are different, broadcast will be applied on the smaller dim.
+// output shape will be something like x.shape[:-1] but with a broadcast dim
+func EucDistance(x, y *G.Node) (retVal *G.Node) {
+	var sub *G.Node
+
+	if x.Dims() == y.Dims() {
+		var sameShape = true
+		for i := 0; i < x.Dims(); i++ {
+			if x.Shape()[i] != y.Shape()[i] {
+				if x.Shape()[i] < y.Shape()[i] {
+					sub = G.Must(G.BroadcastSub(x, y, []byte{byte(i)}, nil))
+				} else {
+					sub = G.Must(G.BroadcastSub(x, y, nil, []byte{byte(i)}))
+				}
+				sameShape = false
+				break
+			}
+		}
+		if sameShape && sub == nil {
+			sub = G.Must(G.Sub(x, y))
+		}
+	}
+	if sub == nil {
+		log.Panicf("x, y shapes not supported: %v, %v", x.Shape(), y.Shape())
+	}
+
+	retVal = G.Must(G.Sqrt(G.Must(G.Sum(G.Must(G.Square(sub)), x.Dims()-1))))
 	return
 }
--- model/din/activation_test.go
+++ model/din/activation_test.go
@@ -27,3 +27,45 @@ func TestPRelu(t *testing.T) {
 		So(output.Value().Data(), ShouldResemble, []float64{-0.1, -0.2, 3, 4})
 	})
 }
+
+func TestEucDistance(t *testing.T) {
+	Convey("euc distance 2 dim", t, func() {
+		g := G.NewGraph()
+		x := G.NodeFromAny(g, tensor.New(tensor.WithShape(2, 4), tensor.WithBacking([]float64{1, 2, -3, 4, -1, 0, -1, 2})), G.WithName("x"))
+		y := G.NodeFromAny(g, tensor.New(tensor.WithShape(2, 4), tensor.WithBacking([]float64{1, 2, -3, 4, 0, 1, 0, 1})), G.WithName("y"))
+		output := EucDistance(x, y)
+		m := G.NewTapeMachine(g)
+		if err := m.RunAll(); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		defer m.Close()
+		So([]int(output.Shape()), ShouldResemble, []int{2})
+		So(output.Value().Data(), ShouldResemble, []float64{0, 2})
+	})
+	Convey("euc distance 3 dim no broadcast", t, func() {
+		g := G.NewGraph()
+		x := G.NodeFromAny(g, tensor.New(tensor.WithShape(3, 2, 2), tensor.WithBacking([]float64{1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0})), G.WithName("x"))
+		y := G.NodeFromAny(g, tensor.New(tensor.WithShape(3, 2, 2), tensor.WithBacking([]float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})), G.WithName("y"))
+		output := EucDistance(x, y)
+		m := G.NewTapeMachine(g)
+		if err := m.RunAll(); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		defer m.Close()
+		So([]int(output.Shape()), ShouldResemble, []int{3, 2})
+		So(output.Value().Data(), ShouldResemble, []float64{1, 0, 1, 1, 0, 1})
+	})
+	Convey("euc distance 3 dim broadcast", t, func() {
+		g := G.NewGraph()
+		x := G.NodeFromAny(g, tensor.New(tensor.WithShape(3, 2, 2), tensor.WithBacking([]float64{0, 2, 0, 0, -1, 1, -1, 1, 1, 2, 2, 2})), G.WithName("x"))
+		y := G.NodeFromAny(g, tensor.New(tensor.WithShape(3, 1, 2), tensor.WithBacking([]float64{0, 1, 0, 1, 1, 2})), G.WithName("y"))
+		output := EucDistance(x, y)
+		m := G.NewTapeMachine(g)
+		if err := m.RunAll(); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		defer m.Close()
+		So([]int(output.Shape()), ShouldResemble, []int{3, 2})
+		So(output.Value().Data(), ShouldResemble, []float64{1, 1, 1, 1, 0, 1})
+	})
+}
